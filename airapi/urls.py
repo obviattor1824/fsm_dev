e836c8a36f137e2f14a357c9b95d09847c67025a
david nabarro <dsnabarro@gmail.com>
2018-09-05 14:15:29 +0200

    both apis connected and sample integration

--- airapi/urls.py
+++
@@ -1,6 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('airtable', views.air_api, name ='air_api'),
-]
--- fsm_dev/urls.py
+++ fsm_dev/urls.py
@@ -18,6 +18,5 @@ from django.urls import include, path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('xerapi.urls')),
-    path('', include('airapi.urls')),
+    path('', include('apis.urls')),
 ]
